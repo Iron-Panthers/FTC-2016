0d6582e40349930b7aa5c6e66d1bfb0c64a9c4e0
Maxim <maximumpwnguin@gmail.com>
2016-09-26 16:54:38 -0700

    Create more hardware

--- FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/DriveMode.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/DriveMode.java
@@ -5,10 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.team7316.util.AxisWrapper;
+import org.firstinspires.ftc.team7316.util.ButtonWrapper;
 import org.firstinspires.ftc.team7316.util.DcMotorWrapper;
 import org.firstinspires.ftc.team7316.util.GamepadAxis;
+import org.firstinspires.ftc.team7316.util.GamepadButton;
 import org.firstinspires.ftc.team7316.util.GamepadWrapper;
 import org.firstinspires.ftc.team7316.util.Scheduler;
+import org.firstinspires.ftc.team7316.util.ServoWrapper;
 
 /**
  * Created by Maxim on 9/26/2016.
@@ -21,6 +24,7 @@ public class DriveMode extends OpMode {
     private GamepadWrapper gpWrapper;
     private AxisWrapper leftAxis, rightAxis;
     private DcMotorWrapper leftDrive, rightDrive;
+    private ServoWrapper servoWrapper;
 
     @Override
     public void init() {
@@ -33,6 +37,8 @@ public class DriveMode extends OpMode {
 
         leftDrive = new DcMotorWrapper(hardware.leftDriveMotor, leftAxis);
         rightDrive = new DcMotorWrapper(hardware.rightDriveMotor, rightAxis);
+
+        //servoWrapper = new ServoWrapper(hardware.leftCatcherServo, new ButtonWrapper(GamepadButton.L_BUMPER, gpWrapper));
 
     }
 
--- FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/Hardware.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/Hardware.java
@@ -16,22 +16,27 @@ public class Hardware {
     private static final String HITTING_MOTOR_NAME = "mh";
     private static final String LEFT_BEACON_SERVO_NAME = "sbl";
     private static final String RIGHT_BEACON_SERVO_NAME = "sbr";
+    private static final String LEFT_CATCHER_SERVO_NAME = "scl";
+    private static final String RIGHT_CATCHER_SERVO_NAME = "scr";
 
-    public DcMotor leftDriveMotor;
-    public DcMotor rightDriveMotor;
+    public DcMotor leftDriveMotor, rightDriveMotor;
     public DcMotor hittingMotor;
-    public Servo leftBeaconServo;
-    public Servo rightBeaconServo;
+    public Servo leftBeaconServo, rightBeaconServo;
+    public Servo leftCatcherServo, rightCatcherServo;
 
 
     public Hardware (HardwareMap map) {
+
         leftDriveMotor = map.dcMotor.get(LEFT_DRIVE_MOTOR_NAME);
         leftDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         rightDriveMotor = map.dcMotor.get(RIGHT_DRIVE_MOTOR_NAME);
         rightDriveMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        hittingMotor = map.dcMotor.get(HITTING_MOTOR_NAME);
-        hittingMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //leftCatcherServo = map.servo.get(LEFT_CATCHER_SERVO_NAME);
+        //rightCatcherServo = map.servo.get(RIGHT_CATCHER_SERVO_NAME);
+        //hittingMotor = map.dcMotor.get(HITTING_MOTOR_NAME);
+        //hittingMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         /*
         leftBeaconServo = map.servo.get(LEFT_BEACON_SERVO_NAME);
