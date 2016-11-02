eecda8849378ca64f29e2ac533e3f235cfa3ff02
Maxim <maximumpwnguin@gmail.com>
2016-11-02 16:07:01 -0700

    Fixed the CRLF errors
    Mac: CR
    Linux: LF
    Windows: Why not both?

--- FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/Alliance.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/Alliance.java
@@ -2,30 +2,30 @@ package org.firstinspires.ftc.team7316.util;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
+/**
+ * Created by Maxim on 10/18/2016.
+ */
+public enum Alliance {
+    RED(30), BLUE(17);
 
-/**  * Created by Maxim on 10/18/2016.  */ 
-public enum Alliance{
+    public final int threshold;
 
-    RED(30), BLUE(17);  
-
-    public int threshold;
-      
-
-    Alliance(int threshold) { 
-        this.threshold = threshold; 
+    Alliance(int threshold) {
+        this.threshold = threshold;
     }
 
-    /** *
-     * Check if good good 
-     * @param sensor the sensor to check if good good 
-     * @return is good good or not is good good 
-     */ 
-
-    public boolean isGoodGood(ColorSensor sensor) { 
-        switch (this) { case RED: return sensor.red() >= threshold; 
-            case BLUE: return sensor.blue() >= threshold; } 
-        throw new IllegalArgumentException("Something wrong happened in enum Alliance even though nothing wrong should have happened"); 
+    /**
+     * Check if good good
+     * @param sensor the sensor to check if good good
+     * @return is good good or not is good good
+     */
+    public boolean isGoodGood(ColorSensor sensor) {
+        switch (this) {
+            case RED:
+                return sensor.red() >= threshold;
+            case BLUE:
+                return sensor.blue() >= threshold;
+        }
+        throw new IllegalArgumentException("Something wrong happened in enum Alliance even though nothing wrong should have happened");
     }
-
-     
-} 
+}
