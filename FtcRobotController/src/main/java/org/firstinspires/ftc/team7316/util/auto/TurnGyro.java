4d7af6616bd5920245a690413caf23a0517526d9
codeWorth <agcummings11@gmail.com>
2016-11-01 21:33:51 -0700

    gyro turn code
    both gyro turn to a delta and gyro to to a specific bearing are both completed, but not tested

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/auto/TurnGyro.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.team7316.util.auto;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+import org.firstinspires.ftc.team7316.util.Loopable;
+
+/**
+ * Created by andrew on 11/1/16.
+ */
+public class TurnGyro implements Loopable {
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
+    private GyroSensor gyro;
+
+    private float remainingBearing;
+
+    protected double power;
+    protected float deltaBearing;
+    protected float startBearing;
+
+
+    public TurnGyro(float deltaBearing, double power, DcMotor leftMotor, DcMotor rightMotor, GyroSensor gyro) { //+power = clockwise
+        this.deltaBearing = deltaBearing;
+        this.power = power;
+        this.leftMotor = leftMotor;
+        this.rightMotor = rightMotor;
+        this.gyro = gyro;
+    }
+
+    @Override
+    public void init() {
+        this.startBearing = this.gyro.getHeading();
+    }
+
+
+    @Override
+    public void loop() {
+        this.leftMotor.setPower(this.power);
+        this.rightMotor.setPower(-this.power);
+        this.remainingBearing = this.deltaBearing - Math.abs(this.gyro.getHeading() - this.startBearing);
+    }
+
+
+    @Override
+    public boolean shouldRemove() {
+        return this.remainingBearing < 0;
+    }
+
+    @Override
+    public void terminate() {
+        this.leftMotor.setPower(0);
+        this.rightMotor.setPower(0);
+    }
+
+}
