d87ed742b9e2c22f7d24b91f0e7e0d5aee2aa627
codeWorth <agcummings11@gmail.com>
2016-11-02 18:06:12 -0700

    Added many things for auto sequence
    -auto codes is a class which contains the different sequences we might want to follow
    -AutoMode runs this command
    -CommandSequence runs a bunch of loopables one after another
    -SimulataneousCommands runs may at once (or as close to at once as possible)
    -Hardware has a bunch of sesnors and the two servos uncommented, so that will need to be fixed
    
    --IMPORTANT--
    I used optical distance sensor as a placeholder, but I am worried about how reliable that will be, given that the beacon lights up in different ways. Maybe we should use the NXT ultrasonic sensor?

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/auto/conditions/DistanceSensorThreshold.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.team7316.util.auto.conditions;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.team7316.util.Loopable;
+
+/**
+ * Created by andrew on 11/2/16.
+ */
+public class DistanceSensorThreshold implements Loopable {
+
+    private Har
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public boolean shouldRemove() {
+        return false;
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+}
--- FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/hardware/Hardware.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/hardware/Hardware.java
@@ -1,9 +1,15 @@
 package org.firstinspires.ftc.team7316.util.hardware;
 
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -30,12 +36,18 @@ public class Hardware {
     private static final String LEFT_CATCHER_SERVO_NAME = "scl";
     private static final String RIGHT_CATCHER_SERVO_NAME = "scr";
     private static final String LIGHT_SENSOR_NAME = "light";
+    private static final String GYRO_SENSOR_NAME = "gyro";
+    private static final String COLOR_SENSOR_NAME = "color";
+    private static final String DISTANCE_SENSOR_NAME = "dist";
 
     public DcMotor leftDriveMotor, rightDriveMotor;
     public DcMotor hittingMotor;
     public Servo leftBeaconServo, rightBeaconServo;
     public Servo leftCatcherServo, rightCatcherServo;
     public LightSensor lightSensor;
+    public GyroSensor gyroSensor;
+    public ColorSensor colorSensor;
+    public OpticalDistanceSensor distanceSensor;
 
     public double jankDelta = 0;
     public double jankSum = 0;
@@ -54,10 +66,13 @@ public class Hardware {
         //hittingMotor = map.dcMotor.get(HITTING_MOTOR_NAME);
         //hittingMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        // leftBeaconServo = map.servo.get(LEFT_BEACON_SERVO_NAME);
-        // rightBeaconServo = map.servo.get(RIGHT_BEACON_SERVO_NAME);
+        leftBeaconServo = map.servo.get(LEFT_BEACON_SERVO_NAME);
+        rightBeaconServo = map.servo.get(RIGHT_BEACON_SERVO_NAME);
 
-        //lightSensor = map.lightSensor.get(LIGHT_SENSOR_NAME);
+        lightSensor = map.lightSensor.get(LIGHT_SENSOR_NAME);
+        gyroSensor = map.gyroSensor.get(GYRO_SENSOR_NAME);
+        colorSensor = map.colorSensor.get(COLOR_SENSOR_NAME);
+        distanceSensor = map.opticalDistanceSensor.get(DISTANCE_SENSOR_NAME);
     }
 
     public static void setHardwareMap(HardwareMap map) {
