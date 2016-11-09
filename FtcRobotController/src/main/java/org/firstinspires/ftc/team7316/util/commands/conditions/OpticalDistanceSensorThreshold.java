70d0c5c7ae78b52159c65bc718b1042479c808f3
codeWorth <agcummings11@gmail.com>
2016-11-08 18:04:22 -0800

    some shooting commands
    added OpticalDistanceSensorThreshold conditional
    added RunMotorUntilCondition

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/commands/conditions/OpticalDistanceSensorThreshold.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.team7316.util.commands.conditions;
+
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+/**
+ * Created by andrew on 11/8/16.
+ */
+public class OpticalDistanceSensorThreshold implements Conditional {
+
+    private OpticalDistanceSensor sensor;
+    private double threshold;
+    private boolean wantedLess;
+
+    public OpticalDistanceSensorThreshold(OpticalDistanceSensor sensor, double thresh, boolean wantedLess) { //if wanted less is true, then it will return true if distance is less than thresh
+        this.sensor = sensor;
+        this.threshold = thresh;
+        this.wantedLess = wantedLess;
+    }
+
+    @Override
+    public boolean shouldRemove() {
+        if (this.wantedLess) {
+            return sensor.getLightDetected() < this.threshold;
+        } else {
+            return sensor.getLightDetected() > this.threshold;
+        }
+    }
+}
