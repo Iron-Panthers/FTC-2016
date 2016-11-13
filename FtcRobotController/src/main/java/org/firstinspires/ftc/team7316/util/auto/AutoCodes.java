a01602d0b2cf849e8a13187cf0bf2bed69d0975e
codeWorth <agcummings11@gmail.com>
2016-11-13 13:25:18 -0800

    added a test for auto

--- FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/auto/AutoCodes.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/auto/AutoCodes.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.team7316.util.auto;
 
 import org.firstinspires.ftc.team7316.util.Alliance;
 import org.firstinspires.ftc.team7316.util.Loopable;
+import org.firstinspires.ftc.team7316.util.auto.conditions.Conditional;
 import org.firstinspires.ftc.team7316.util.auto.conditions.DistanceSensorThreshold;
 import org.firstinspires.ftc.team7316.util.hardware.Hardware;
 
@@ -10,10 +11,20 @@ import org.firstinspires.ftc.team7316.util.hardware.Hardware;
  */
 public class AutoCodes {
     private static CommandSequence closeBeaconCloseStartRed;
+    private static CommandSequence darrionHouseTest;
 
     public static SimultaneousCommands robotDriveDistanceAccurate(double distance, double power) {
         DriveDistanceAccurate leftMotor = new DriveDistanceAccurate(distance, power, Hardware.instance.leftDriveMotor);
         DriveDistanceAccurate rightMotor = new DriveDistanceAccurate(distance, power, Hardware.instance.rightDriveMotor);
+        Loopable[] both = {leftMotor, rightMotor};
+
+        SimultaneousCommands bothDrive = new SimultaneousCommands(both);
+        return bothDrive;
+    }
+
+    public static SimultaneousCommands robotDriveUntilCondition(Conditional conditional, double power) {
+        DriveUntilCondition leftMotor = new DriveUntilCondition(power, Hardware.instance.leftDriveMotor, conditional);
+        DriveUntilCondition rightMotor = new DriveUntilCondition(power, Hardware.instance.rightDriveMotor, conditional);
         Loopable[] both = {leftMotor, rightMotor};
 
         SimultaneousCommands bothDrive = new SimultaneousCommands(both);
@@ -25,9 +36,9 @@ public class AutoCodes {
 
             SimultaneousCommands driveToLine = AutoCodes.robotDriveDistanceAccurate(11287, 0.5);
 
-            Loopable turnToLine = new TurnGyro(0, 0.2, Hardware.instance.leftDriveMotor, Hardware.instance.rightDriveMotor, Hardware.instance.gyroSensor);
+            Loopable turnToLine = new SetBearingGyro(0, 0.2, Hardware.instance.leftDriveMotor, Hardware.instance.rightDriveMotor, Hardware.instance.gyroSensor);
 
-            Loopable distanceCondition = new DistanceSensorThreshold(Hardware.instance.distanceSensor, 1.5, true);
+            Conditional distanceCondition = new DistanceSensorThreshold(Hardware.instance.distanceSensor, 1.5, true);
             Loopable followLine = new LineFollowUntilCondition(Hardware.instance.leftDriveMotor, Hardware.instance.rightDriveMotor, Hardware.instance.lightSensor, 0.1, distanceCondition);
 
             Loopable pressBeacon = new PressBeacon(Alliance.RED, Hardware.instance.colorSensor, Hardware.instance.leftBeaconServo, Hardware.instance.rightBeaconServo, true);
@@ -38,6 +49,27 @@ public class AutoCodes {
         }
 
         return AutoCodes.closeBeaconCloseStartRed;
+    }
+
+    public static CommandSequence darrionHouseTest() {
+        if (AutoCodes.darrionHouseTest == null) {
+            SimultaneousCommands forward = AutoCodes.robotDriveDistanceAccurate(10000, 0.6);
+
+            Loopable turn = new TurnGyro(90, 0.3, Hardware.instance.leftDriveMotor, Hardware.instance.rightDriveMotor, Hardware.instance.gyroSensor);
+
+            Conditional condition = new DistanceSensorThreshold(Hardware.instance.distanceSensor, 10, true);
+            SimultaneousCommands driveUntilWall = AutoCodes.robotDriveUntilCondition(condition, 0.3);
+
+            SimultaneousCommands back = AutoCodes.robotDriveDistanceAccurate(7000, -0.5);
+
+            Loopable turnBack = new TurnGyro(-90, 0.3, Hardware.instance.leftDriveMotor, Hardware.instance.rightDriveMotor, Hardware.instance.gyroSensor);
+
+            Loopable[] cmds = {forward, turn, driveUntilWall, back, forward};
+
+            AutoCodes.darrionHouseTest =  new CommandSequence(cmds);
+        }
+
+        return AutoCodes.darrionHouseTest;
     }
 
 }
---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/auto/DriveUntilCondition.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.team7316.util.auto;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.team7316.util.Loopable;
+import org.firstinspires.ftc.team7316.util.auto.conditions.Conditional;
+
+/**
+ * Created by andrew on 11/11/16.
+ */
+public class DriveUntilCondition extends DriveDistance implements Loopable {
+
+    private double power;
+    private DcMotor motor;
+    private Conditional condition;
+
+    public DriveUntilCondition(double power, DcMotor motor, Conditional condition) {
+        super(0, power, motor);
+        this.condition = condition;
+        this.power = power;
+        this.motor = motor;
+    }
+
+    @Override
+    public boolean shouldRemove() {
+        return condition.shouldRemove();
+    }
+
+
+}
