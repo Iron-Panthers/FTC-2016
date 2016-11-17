e882318413eee2bfd99fbb190a506f088e79d754
Maxim <maximumpwnguin@gmail.com>
2016-11-16 16:21:59 -0800

    Add exponentials to AxisWrapper

--- FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/input/AxisWrapper.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/util/input/AxisWrapper.java
@@ -7,14 +7,22 @@ public class AxisWrapper {
 
     private GamepadAxis inputName;
     private GamepadWrapper gpWrapper;
+    private double power;
 
-    public AxisWrapper (GamepadAxis inputName, GamepadWrapper gpWrapper) {
+    public AxisWrapper(GamepadAxis inputName, GamepadWrapper gpWrapper, double power) {
         this.inputName = inputName;
         this.gpWrapper = gpWrapper;
+        this.power = power;
+    }
+
+    public AxisWrapper(GamepadAxis inputName, GamepadWrapper gpWrapper) {
+        this(inputName, gpWrapper, 1);
     }
 
     public float value() {
-        return gpWrapper.axisValue(inputName);
+        float input = gpWrapper.axisValue(inputName);
+        float value = (float) Math.abs(Math.pow(input, power));
+        return input > 0 ? value : -value;
     }
 
     public String name() {
