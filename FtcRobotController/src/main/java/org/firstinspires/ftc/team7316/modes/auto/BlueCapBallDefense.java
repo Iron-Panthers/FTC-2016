cf1ffc5c1e6c17590654afcadcc3cea1d3673882
codeWorth <agcummings11@gmail.com>
2017-02-16 19:55:19 -0800

    Create op modes for defense

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/team7316/modes/auto/BlueCapBallDefense.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.team7316.modes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.team7316.modes.CommandAuto;
+import org.firstinspires.ftc.team7316.util.Loopable;
+import org.firstinspires.ftc.team7316.util.commands.AutoCodes;
+
+/**
+ * Created by andrew on 2/16/17.
+ */
+@Autonomous(name = "Blue Cap Ball Defense")
+public class BlueCapBallDefense extends CommandAuto {
+    @Override
+    protected Loopable getTask() {
+        return AutoCodes.bluePushYogaBall();
+    }
+}
